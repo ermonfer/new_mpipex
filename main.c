839798d57aa063193245977b52b7be6eb72f7e62
Fernando Montero Erustes <fmontero@c1r11s4.42madrid.com>
2025-06-18 19:50:42 +0200

    servidores

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fmontero <fmontero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 17:39:30 by fmontero          #+#    #+#             */
-/*   Updated: 2025/06/18 17:59:42 by fmontero         ###   ########.fr       */
+/*   Updated: 2025/06/18 19:50:10 by fmontero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,44 @@
 int		execve_p(char **envp);
 int 	ft_waiting_childs_return(pid_t childs[2]);
 int		ft_init_pipex(t_cmd_data *cmds, int argc, char **argv, char **enpv);
+int		ft_init_pipex();
 int 	ft_child1_exec(t_pipex_fds fds, t_cmd_data cmds);
 int		ft_open_fds(t_pipex_fds *fds, int argc, char **argv);
 void	ft_check_args_number(int argc);
 void	ft_check_infile_outfile(int argc, char *argv);
-void	ft_child_1(t_pipex_fds *fds, t_cmd_data *cmds);
-void	ft_child_2(t_pipex_fds *fds, t_cmd_data *cmds);
+void	ft_child_1(char *cmd_str, char **envp);
+void	ft_child_2(char *cmd_str, char **envp);
 
 
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_pipex_fds		fds;
+	pid_t			childs[2];
+	int				status;
 
 	ft_check_args_number(argc);
-	ft_get_cmds_data(cmds, argc, argv, envp);
 	if (pipe(fds.pipe_fds) == -1)
 	{
-		print_errors("pipe");
-		ft_free_cmds_data(cmds, 2);
-		return (PIPE_ERROR);
+		ft_print_error("pipex: pipe", 0);
+		return (1);
+	}
+	childs[0] = fork();
+	if (childs[0] == -1)
+		ft_print_error("pipex: fork", 0);
+	if (childs[0] == 0)
+		ft_child_1(argv[2], envp);
+	childs[1] = fork();
+	if (childs[1] == -1)
+		ft_print_error("pipex: fork", 0);
+	if (childs[1] == 0)
+		ft_child_2(argv[3], envp);
+	close(fds.pipe_fds[0]);
+	close(fds.pipe_fds[1]);
+	if (childs[0] != -1)
+		waitpid(childs[0], NULL, 0);
+	if (childs[1] != -1)
+		waitpid(childs[1], NULL, 0);
+	return (status);
 	}
 
 
@@ -45,32 +64,6 @@ int	main(int argc, char *argv[], char *envp[])
 	/* Imprimir los errores en el caso de haberlos*/
 }
 
-int	ft_init_pipex(t_cmd_data *cmds, int argc, char **argv, char **enpv)
-{
-	t_pipex_fds		*fds;
-	int				pid1;
-	int				pid2;
-
-	fds = malloc(sizeof(t_pipex_fds));
-	if (fds == NULL)
-		return (ERR_MALLOC);
-	if (ft_open_fd(fds, argc, argv) != SUCCESS)
-		return (ft_free_fds(fds, j));
-	pid1 = fork();
-	if (pid1 < 0)
-		return (ft_free_fds (fds, FORK_ERROR));
-	if (pid1 == 0)
-		ft_child_1(cmds, fds, enpv);
-	pid2 = fork ();
-	if (pid2 < 0)
-		return (ft_free_fds (fds, FORK_ERROR));
-	if (pid2 == 0)
-		ft_child_2(cmds, fds, enpv);
-	ft_free_fds(fds, SUCCESS);
-	//waitpid(pid1, NULL, 0);
-	//waitpid(pid2, NULL, 0);
-	return (SUCCESS);
-}
 
 int	execve_p(char **envp)
 {
